63a418539da0e3964409de30aede0c34e92db507
zaid <zaidvora58@gmail.com>
2024-08-01 22:28:40 +1000

    commit

---
+++ Week-3/public/js/script.js
@@ -0,0 +1,8 @@
+const clickMe = () =>{
+    alert("Thanks for clicking me. Hope you have a nice day!")
+}
+$(document).ready(function () {
+    $('#clickMeButton').click(() => {
+        clickMe();
+    })
+})
